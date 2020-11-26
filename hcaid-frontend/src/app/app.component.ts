205bbc23b1d311f5717220a41b00313a5686f302
Ian Vink <ninjaws@protonmail.com>
2020-11-26 18:22:45 +0100

    [Updated] Icon, Title, Terms of service, etc

--- hcaid-frontend/src/app/app.component.ts
+++ hcaid-frontend/src/app/app.component.ts
@@ -4,11 +4,10 @@ import { Title } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-
   constructor(private title: Title) {
-    title.setTitle('Test');
-}
+    title.setTitle('Mushroom Poison Predictor');
+  }
 }
--- hcaid-frontend/src/app/home/home.component.ts
+++ hcaid-frontend/src/app/home/home.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  quizStarted = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  startQuiz() {
+    if (!this.quizStarted) this.quizStarted = true;
   }
-
 }
